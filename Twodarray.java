28961ec56870746d8acf8db31137bda5064f89ed
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-09 15:01:23 +0530

    Add files via upload

---
+++ Twodarray.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Twodarray{
+    public static void main(String args[]){
+        //intitialization of 2D array
+        int a[][]={{1,2},{3,4}};
+        //accessing 1d array in 2D array
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
